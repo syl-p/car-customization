110d8270c9d38ee937c9cacf2c72073482ba0287
Sylvain Pastor <sylvain@MBPdeatnosadmin.lan>
2023-12-02 23:21:04 +0100

    fix: fix typo error in config file

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-<import { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
@@ -6,4 +6,3 @@ export default defineConfig({
   plugins: [react()],
   base: '/car-customization/'
 })
->
